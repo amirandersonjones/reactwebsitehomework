66d60fa6ed06023393c3e032e87d806645a296ff
amirandersonjones <amirandersonjones@gmail.com>
2022-03-30 12:23:28 -0500

    drinksrendering

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,21 +2,21 @@ import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
 
 //function definition for my component-return a single JSX element
-const Navbar = () => {
+const Navbar = props => {
     useEffect (() => {console.log('Navbar component redered or re-rendered!')});
 
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">AMIR'S SHOP</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#">AMIR'S SHOP</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <Link class="nav-item nav-link active" to="/">Home <span class="sr-only">(current)</span></Link>
-      <Link class="nav-item nav-link" to="/Shop">Shop</Link>
-      <Link class="nav-item nav-link" to="/Gallery">Gallery</Link>
-      <Link class="nav-item nav-link disabled" to="#">Disabled</Link>
+  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div className="navbar-nav">
+      <Link className="nav-item nav-link active" to="/">Home <span className="sr-only">(current)</span></Link>
+      <Link className="nav-item nav-link" to="/Shop">Shop</Link>
+      <Link className="nav-item nav-link" to="/Gallery">Gallery</Link>
+      <Link className="nav-item nav-link disabled" to="#">Disabled</Link>
     </div>
   </div>
 </nav>
--- src/views/Shop.js
+++ src/views/Shop.js
@@ -20,35 +20,51 @@ const Shop = () => {
    //make api call
    const getDrinkData = async () =>{
         let response = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail')
-        return response.status === 2000 ? response.data : null
+        return response.status === 200 ? response.data : null
    }
 
     //loading the api call response data into our state variable
    const loadDrinkData = async () =>{
         let data = await getDrinkData();
-        console.log(data, typeof data);
+        console.log(data.drinks, typeof data.drinks);
         //take this data and set our state variable with it
-        setDrinks(data);
+        setDrinks(data.drinks);
     }
    
     //state variable setup
     const [drinks, setDrinks] = useState(() => loadDrinkData());
 
-    return(
+    return (
         <div className="container">
-        <div className="row">
-           <h1>AMIR'S COCKTAIL AND SALOON</h1>
+        <div className="row justify-content-center">
+            <h1>AMIR'S COCKTAIL AND SALOON</h1>
         </div>
         <div className="row">
-           {/* cards for each DRINK once the DRINKs have actually laoded*/}
-           <h1>Stirring up drinks! Please hold...</h1>
+            {/* cards for each animal once the animals have actually loaded */ console.log(drinks, typeof drinks)}
+            {typeof drinks === 'object' && !drinks.then ? drinks.map((drink, index) => {
+                 return <div key={index} className="card m-3" style={{width: 18 + 'rem'}}>
+                    <img src={drink.strDrinkThumb} className="card-img-top" alt="" />
+                    <div className="card-body">
+                        <h5 className="card-title">dfdf</h5>
+                        <h5 className="card-title font-italic">cdfdf</h5>
+                        <p className="card-text">dfadfds</p>
+                    </div>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">gg</li>
+                        <li className="list-group-item">afdf</li>
+                        <li className="list-group-item"><span className="float-left">fafd</span><span className="float-right">dfaf</span></li>
+                    </ul>
+                    <div className="card-body"> 
+                        {/* <p className="card-link"><span className="float-left">${animal.price.toFixed(2)}</span><span className="float-right btn btn-sm btn-secondary" onClick={() => adoptAnimal(animal)}>Adopt</span></p> */}
+                    </div>
+                </div>
+            }) :
+                <h1>Stirring up cocktails! Please hold...</h1>
+            }
         </div>
     </div>
-    );          
+);
 }
-
-
-
 
 
 
